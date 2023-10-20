0e98691ca6cb6359666e711b0591327b17e25ae5
Vinay Joshi <joshivinay@berkeley.edu>
2023-10-19 19:16:32 -0700

    git rm for moved files

--- oftra/__init__.py
+++ oftra/__init__.py
@@ -1,1 +1,9 @@
-"""Init file"""
+from oftra.core.config.oftra_config import OftraConfig
+from oftra.core.context.application_context import ApplicationContext
+from oftra.core.workflow.node import Node
+
+__all__ = [
+  "ApplicationContext",
+  "OftraConfig",
+  "Node"
+]
--- oftra/oftra_app.py
+++ oftra/oftra_app.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
-from config import oftra_config
-from oftra.context.oftra_context import OftraContext
-from workflow import workflow
+from oftra.spark.spark_application_context import SparkApplicationContext
+from oftra import OftraConfig, ApplicationContext
+from oftra.spark.spark_workflow import SparkWorkflow
 
 
 class OftraApp:
@@ -15,23 +15,26 @@ class OftraApp:
     parser.add_argument("-W", "--workflow-file", help="Workflow DAG file to execute", default=None, metavar="FILE")
     parser.add_argument("-C", "--config-file", help="External Variables and configurations as file", default=None, metavar="FILE")
     args = parser.parse_args()
+    
+    
+    # Create the config object    
+    config = OftraConfig(args.config_file)
 
-    # Create the context    
-    context = OftraContext(args.config_file)
+    # Create the application context. Depending on the type of configuration, create the appropriate application context
+
+    context: ApplicationContext = SparkApplicationContext(config)
         
     # Check if the workflow file is provided
     if args.workflow_file is None:
       print("Workflow file is not provided")
       return
 
-    # Load the workflow DAG from the workflow file. In future it could be a workflow string
-    workflow1 = workflow.Workflow(args.workflow_file)
-
-    # Load the external variables and configurations into a configuration object
-    # config = oftra_config.OftraConfig(args.config_file)
+    # Create a workflow of the appropriate type
+    app_workflow = SparkWorkflow(config, args.workflow_file)
 
     # Execute the workflow DAG
-    workflow1.execute(context)
+    app_workflow.execute(context)
+
     
 
 
